c34cbfa17d7853ffd7a7ed4b4b9e30024a3f1aad
vagrant <vagrant@homestead>
2019-02-21 09:24:20 +0000

    email verify

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -3,10 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Auth;
 
 class PagesController extends Controller
 {
-    public function root()
+    public function root(Request $request)
     {
         return view('pages.root');
     }
