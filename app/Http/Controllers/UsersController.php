b1ae5cf9fd4caac008a565dd17e53bd5404ec568
vagrant <vagrant@homestead>
2019-02-27 08:09:02 +0000

    edit profile

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -16,4 +16,22 @@ class UsersController extends Controller
     {
         return view('users.show', compact('user'));
     }
+
+    /**
+     * 用户编辑
+     * @param User $user
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function edit(User $user)
+    {
+        return view('users.edit', compact('user'));
+    }
+
+
+    public function update(Request $request, User $user)
+    {
+        $user->update($request->all());
+
+        return redirect()->route('users.show', $user->id)->with('success', '个人资料更新成功');
+    }
 }
