ba794467793649c8e5281c8fd7a03597001e3c2f
vagrant <vagrant@homestead>
2019-04-03 03:08:16 +0000

    reply store

--- routes/api.php
+++ routes/api.php
@@ -75,6 +75,9 @@ $api->version('v1',['namespace'=>'App\Http\Controllers\Api', 'middleware'=>['ser
             //删除话题
             $api->delete('topics/{topic}', 'TopicsController@destroy')->name('api.topics.destroy');
 
+            //发布回复
+            $api->post('topics/{topic}/replies', 'RepliesController@store')->name('api.topics.replies.store');
+
 
         });
 
